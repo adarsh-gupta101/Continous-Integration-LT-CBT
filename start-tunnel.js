92174c2eb84472669e34b543e0d79445cabc1ec7
adarsh-gupta101 <adarshgupta2468@gmail.com>
2024-12-18 23:32:40 +0530

    LT tunnel setup

---
+++ start-tunnel.js
@@ -0,0 +1,21 @@
+const lambdaTunnel = require("@lambdatest/node-tunnel");
+
+// Replace these with your LambdaTest credentials
+const tunnelArguments = {
+  user: process.env.LT_USERNAME || "<lambdatest-user>",
+  key: process.env.LT_ACCESS_KEY || "<lambdatest-accesskey>",
+};
+
+// Start the tunnel
+(async function () {
+  try {
+    const tunnelInstance = new lambdaTunnel();
+    const isTunnelStarted = await tunnelInstance.start(tunnelArguments);
+    if (isTunnelStarted) {
+      console.log("Tunnel is Running Successfully");
+      // Run your tests here
+    }
+  } catch (error) {
+    console.error("Error starting the tunnel:", error);
+  }
+})();
